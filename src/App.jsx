87067be2a4c24098b72018d3a08032bfb74aa6f8
Manav karwal <haigamekhelte@gmail.com>
2025-01-18 18:16:21 +0530

    local storage  auth provider login setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from '../src/components/Auth/Login'
 import EmployeeDashBoard from './components/DashBoard/EmployeeDashBoard'
 import AdminDashBoard from './components/DashBoard/AdminDashBoard'
-import { use } from 'react'
+import { AuthContext } from './context/AuthProvider'
 import { getLocalStorage, setLocalStorage } from './utils/LocalStorage'
 
 const App = () => {
-    useEffect(()=>{
-      // setLocalStorage()
-      getLocalStorage()
-    },)
+  useEffect(() => {
+    setLocalStorage()
+    getLocalStorage()
+  },)
+
+  const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext);
+
+  useEffect(() => {
+    if (authData) {
+      const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"))
+      if (loggedInUser) {
+        setUser(loggedInUser.role)
+      }
+    }
+  },[ authData])
+
+
+
+  const handleLogin = (email, password) => {
+
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (authData && authData.employees.find((e) => email == e.email && e.password == password)) {
+      setUser('employee')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee' }))
+    } else {
+      alert("Invalid Credentials")
+    }
+
+  }
+
+
 
   return (
-    <div>
-     <Login/>
-     {/* <EmployeeDashBoard/> */}
-     {/* <AdminDashBoard/> */}
-    </div>
+    <>
+      {!user ? <Login handleLogin={handleLogin} /> : user == 'admin' ? <AdminDashBoard /> : <EmployeeDashBoard />
+      }
+
+    </>
   )
 }
 
--- src/utils/LocalStorage.jsx
+++ src/utils/LocalStorage.jsx
@@ -202,8 +202,10 @@ export const setLocalStorage = () => {
 }
 
 export const getLocalStorage = () => {
-const employee = JSON.parse(localStorage.getItem('employees'))
+const employees = JSON.parse(localStorage.getItem('employees'))
 const admin = JSON.parse(localStorage.getItem('admin'))
 
-console.log(employees, admin)
+return({employees, admin})
+
+
 }
